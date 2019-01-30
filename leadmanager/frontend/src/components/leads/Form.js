5c1bd8ba069169d247162196dfd2bcaf5596f1b3
Jed Diaz <jemjemzms@gmail.com>
2019-01-30 20:18:41 +1300

    Adding Leads
    Adding the Add Leads

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,13 +1,77 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../store/actions/leads";
 
 export class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
+  };
+
   render() {
+    const { name, email, message } = this.state;
     return (
-      <div>
-        <h1>Add Lead Form</h1>
+      <div className="card card-body mt-4 mb-4">
+        <h1>Add Lead</h1>
+        <form onSubmit={this.onSubmitHandler}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={this.onChangeHandler}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              onChange={this.onChangeHandler}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              type="text"
+              name="message"
+              onChange={this.onChangeHandler}
+              value={message}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default Form;
+export default connect(
+  null,
+  { addLead }
+)(Form);
